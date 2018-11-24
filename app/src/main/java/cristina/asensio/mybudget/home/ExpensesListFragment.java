39703da26ef9ced332b48edca13c5ef10b993dbd
casensio <cristina.asensio.munoz@gmail.com>
2018-11-24 16:39:50 +0100

    Created Expense list adapter and displayed data from database in ExpensesListFragment

--- app/src/main/java/cristina/asensio/mybudget/home/ExpensesListFragment.java
+++ app/src/main/java/cristina/asensio/mybudget/home/ExpensesListFragment.java
@@ -1,19 +1,27 @@
 package cristina.asensio.mybudget.home;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import cristina.asensio.mybudget.R;
+import cristina.asensio.mybudget.adapter.ExpenseListAdapter;
+import cristina.asensio.mybudget.database.Expense;
+import cristina.asensio.mybudget.database.ExpenseViewModel;
 
 public class ExpensesListFragment extends Fragment {
 
@@ -30,12 +38,24 @@ public class ExpensesListFragment extends Fragment {
     TextView totalBudgetTextView;
 
     private Unbinder unbinder;
+    private ExpenseViewModel mExpenseViewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.screen_list, container, false);
         unbinder = ButterKnife.bind(this, view);
+        final ExpenseListAdapter adapter = new ExpenseListAdapter(getActivity());
+        listView.setAdapter(adapter);
+        listView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mExpenseViewModel = ViewModelProviders.of(getActivity()).get(ExpenseViewModel.class);
+        mExpenseViewModel.getAllExpenses().observe(getActivity(), new Observer<List<Expense>>() {
+            @Override
+            public void onChanged(@Nullable List<Expense> expenses) {
+                adapter.setExpenses(expenses);
+            }
+        });
         return view;
     }
 
--- app/src/main/java/cristina/asensio/mybudget/home/MainActivity.java
+++ app/src/main/java/cristina/asensio/mybudget/home/MainActivity.java
@@ -1,12 +1,7 @@
 package cristina.asensio.mybudget.home;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ListFragment;
-import android.util.AttributeSet;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
