f9402f9044f456d0916d6d04698ba215637b894f
casensio <cristina.asensio.munoz@gmail.com>
2019-02-02 18:30:50 +0100

    saving max amount available for mounth in settings/preferences

--- app/src/main/java/cristina/asensio/mybudget/home/PreferencesFragment.java
+++ app/src/main/java/cristina/asensio/mybudget/home/PreferencesFragment.java
@@ -1,5 +1,6 @@
 package cristina.asensio.mybudget.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +18,11 @@ import cristina.asensio.mybudget.R;
 
 public class PreferencesFragment extends Fragment implements View.OnClickListener {
 
+    private final String PREFERENCES_KEY = "my_budget_preferences";
+    private final String MAX_AMOUNT_TO_SPEND_A_MONTH_KEY = "max_amount_to_spend";
+    private final float MAX_AMOUNT_TO_SPEND_A_MONTH_DEFAULT = 600;
+    private final int PRIVATE_MODE = 0;
+
     @BindView(R.id.preferences_max_amount)
     EditText preferencesMaxAmountEditText;
 
@@ -24,12 +30,14 @@ public class PreferencesFragment extends Fragment implements View.OnClickListene
     Button savePreferencesButton;
 
     private Unbinder unbinder;
+    private SharedPreferences mSharedPreferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.preferences, container, false);
         unbinder = ButterKnife.bind(this, view);
+        mSharedPreferences = getActivity().getSharedPreferences(PREFERENCES_KEY, PRIVATE_MODE);
         savePreferencesButton.setOnClickListener(this);
         return view;
     }
@@ -46,7 +54,22 @@ public class PreferencesFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onClick(View view) {
+        storeMaxAmountToSpendAMonth();
         goBackToExpensesList();
+    }
+
+    private void storeMaxAmountToSpendAMonth() {
+        final float maxAmountToSpendPerMonth = Float.parseFloat(preferencesMaxAmountEditText.getText().toString());
+        final SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        if (maxAmountToSpendPerMonth != 0) {
+            editor.putFloat(MAX_AMOUNT_TO_SPEND_A_MONTH_KEY, maxAmountToSpendPerMonth);
+
+        } else {
+            editor.putFloat(MAX_AMOUNT_TO_SPEND_A_MONTH_KEY, MAX_AMOUNT_TO_SPEND_A_MONTH_DEFAULT);
+        }
+
+        editor.commit();
     }
 
     private void goBackToExpensesList() {
