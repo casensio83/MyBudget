8bc41018abcd7055cc289f51ebca1f2f4c074858
casensio <cristina.asensio.munoz@gmail.com>
2019-02-02 13:02:47 +0100

    Updated database test

--- app/src/androidTest/java/cristina/asensio/mybudget/database/ExpenseDaoTest.java
+++ app/src/androidTest/java/cristina/asensio/mybudget/database/ExpenseDaoTest.java
@@ -43,40 +43,40 @@ public class ExpenseDaoTest {
 
     @Test
     public void insertAndGetExpense() throws Exception {
-        Expense expense = new Expense("11/11/2018", "100", "Transport", "");
+        Expense expense = new Expense("11/11/2018", 100, "Transport", "");
         mExpenseDao.insert(expense);
-        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getAlphabetizedExpenses());
+        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getExpenses());
         assertEquals(allExpenses.get(0).getTitle(), expense.getTitle());
     }
 
     @Test
     public void getAllExpenses() throws Exception {
-        Expense expense = new Expense("11/11/2018", "100", "Transport", "");
+        Expense expense = new Expense("11/11/2018", 100, "Transport", "");
         mExpenseDao.insert(expense);
-        Expense expense2 = new Expense("12/11/2018", "200", "House", "");
+        Expense expense2 = new Expense("12/11/2018", 200, "House", "");
         mExpenseDao.insert(expense2);
-        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getAlphabetizedExpenses());
+        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getExpenses());
         assertEquals(allExpenses.get(0).getTitle(), expense.getTitle());
         assertEquals(allExpenses.get(1).getTitle(), expense2.getTitle());
     }
 
     @Test
     public void deleteAll() throws Exception {
-        Expense expense = new Expense("11/11/2018", "100", "Transport", "");
+        Expense expense = new Expense("11/11/2018", 100, "Transport", "");
         mExpenseDao.insert(expense);
-        Expense expense2 = new Expense("12/11/2018", "200", "House", "");
+        Expense expense2 = new Expense("12/11/2018", 200, "House", "");
         mExpenseDao.insert(expense2);
         mExpenseDao.deleteAll();
-        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getAlphabetizedExpenses());
+        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getExpenses());
         assertTrue(allExpenses.isEmpty());
     }
 
     @Test
     public void deleteOneExpense() throws Exception {
-        Expense expense = new Expense("11/11/2018", "100", "Transport", "");
+        Expense expense = new Expense("11/11/2018", 100, "Transport", "");
         mExpenseDao.insert(expense);
         mExpenseDao.delete(expense);
-        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getAlphabetizedExpenses());
+        List<Expense> allExpenses = LiveDataTestUtil.getValue(mExpenseDao.getExpenses());
         assertTrue(allExpenses.isEmpty());
     }
 
