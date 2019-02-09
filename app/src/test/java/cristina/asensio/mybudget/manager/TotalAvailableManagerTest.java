570d7a69593c0bf489916b0ac0fb2020edc01ee0
casensio <cristina.asensio.munoz@gmail.com>
2019-02-09 11:38:34 +0100

    Test added for TotalAvailableManager

--- app/src/test/java/cristina/asensio/mybudget/manager/TotalAvailableManagerTest.java
+++ app/src/test/java/cristina/asensio/mybudget/manager/TotalAvailableManagerTest.java
@@ -1,9 +1,12 @@
 package cristina.asensio.mybudget.manager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,25 +14,41 @@ import java.util.List;
 import cristina.asensio.mybudget.database.Expense;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@RunWith(JUnit4.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TotalAvailableManagerTest {
 
+    private final float DEFAULT_AMOUNT = 600;
+
     private List<Expense> mExpenses;
+    private SharedPreferences mSharedPreferences;
+    private Context mContext;
 
     @Before
     public void init() {
         mExpenses = Arrays.asList(
                 new Expense("", 100, "", ""),
                 new Expense("", 50, "", ""));
+        mSharedPreferences = mock(SharedPreferences.class);
+        mContext = mock(Context.class);
     }
 
+    @Test
+    public void getUpdatedTotalAvailable_newExpenseAdded_newExpenseAddedToTotalAvailable() {
+        String result = TotalAvailableManager.getUpdatedTotalAvailable(mExpenses, DEFAULT_AMOUNT, 0);
+        assertThat(result, is("700.00"));
+    }
 
     @Test
-    public void totalAvailableManager_addsNewExpense_newExpenseAddedToTotalAvailable() {
-        String result = TotalAvailableManager.getUpdatedTotalAvailable(mExpenses, 600, 0);
-        assertThat(result, is("700.0"));
+    public void calculateTotalAvailable_applicationIsInstalled_maxAmountIsDefault() {
+        when(mSharedPreferences.getFloat(anyString(), anyFloat())).thenReturn(DEFAULT_AMOUNT);
+        float total = TotalAvailableManager.calculateTotalAvailable(mSharedPreferences);
+        assertThat(total, is(DEFAULT_AMOUNT));
     }
 
 }
